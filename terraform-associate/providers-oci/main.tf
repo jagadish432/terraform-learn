1b30f1a4d062c8f24b8672849a3577eb5455c0ec
Jagadeesh Dachepalli <jagadish.dachepalli@gmail.com>
2022-11-25 22:30:48 +0530

    Providers - OCI - modules
    1. Unfortuantely, not working, even though sending all required params for subnet module it is still failing. Need to look at this later.

--- terraform-associate/providers-oci/main.tf
+++ terraform-associate/providers-oci/main.tf
@@ -25,45 +25,40 @@ provider "oci" {
   config_file_profile = "personal"
 }
 
-resource "oci_core_vcn" "vcn_tf" {
-  dns_label      = "internal"
-  cidr_block     = "10.0.0.0/16"
+# resource "oci_core_vcn" "vcn_tf" {
+#   dns_label      = "internal"
+#   cidr_block     = "10.0.0.0/16"
+#   compartment_id = var.compartment_id
+#   display_name   = "My internal VCN - TF"
+# }
+
+# resource "oci_core_subnet" "subnet_tf" {
+#   vcn_id                     = oci_core_vcn.vcn_tf.id
+#   cidr_block                 = "10.0.1.0/24"
+#   compartment_id             = var.compartment_id
+#   display_name               = "subnet - TF"
+#   prohibit_public_ip_on_vnic = false
+#   dns_label                  = "dev"
+# }
+
+# data "oci_identity_availability_domains" "ads" {
+#   compartment_id = var.compartment_id
+# }
+
+module "vcn" {
+  source  = "oracle-terraform-modules/vcn/oci"
+  version = "3.5.3"
+  # insert the 1 required variable here
   compartment_id = var.compartment_id
-  display_name   = "My internal VCN - TF"
 }
 
-resource "oci_core_subnet" "subnet_tf" {
-  vcn_id                     = oci_core_vcn.vcn_tf.id
-  cidr_block                 = "10.0.1.0/24"
-  compartment_id             = var.compartment_id
-  display_name               = "subnet - TF"
-  prohibit_public_ip_on_vnic = false
-  dns_label                  = "dev"
+module "subnet" {
+  source  = "Terraform-Modules-Lib/subnet/oci"
+  version = "0.0.13"
+  # insert the 4 required variables here
+  name = "subnet_tf"
+  vcn_id = module.vcn.vcn_id
+  public = true
+  cidr = "10.0.0.0/24"
+  acl = { "egress": {}, "ingress": {} }
 }
-
-data "oci_identity_availability_domains" "ads" {
-  compartment_id = var.compartment_id
-}
-
-resource "oci_core_instance" "ubuntu_instance" {
-    # Required
-    availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
-    compartment_id = var.compartment_id
-    shape = "VM.Standard.E2.1.Micro"
-    source_details {
-        source_id = var.ubu2004_image_source_ocid
-        source_type = "image"
-    }
-
-    # Optional
-    display_name = "testing_via_tf"
-    create_vnic_details {
-        assign_public_ip = true
-        subnet_id = oci_core_subnet.subnet_tf.id
-    }
-    # metadata = {
-    # ssh_authorized_keys = file("/Users/jdachepa/.oci/oci_api_key_public.pem")
-	# }
-    preserve_boot_volume = false
-}
-
